4d83085df56ad9b6846a53aa170394558edccfe6
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-09-08 12:06:17 +0530

    added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,46 +1,55 @@
-import React from 'react'
-import bg from '../assets/bg1.avif'
-import Navbar from './Navbar'
-import { Link } from 'react-scroll'
-import {TypeAnimation} from 'react-type-animation'
+import React from 'react';
+import bg from '../assets/bg1.avif';
+import Navbar from './Navbar';
+import { Link } from 'react-scroll';
+import { TypeAnimation } from 'react-type-animation';
+
 const Home = () => {
   return (
-    <header name="Home" style={{
-        width:"100vw",
-        // height:"100vh",
-        backgroundImage:`url(${bg})`,
+    <header
+      name="Home"
+      style={{
+        width: '100vw',
+        backgroundImage: `url(${bg})`,
         backgroundSize: 'cover',
-        backgroundPosition:"center",
-        backgroundRepeat:"no-repeat",
-       
-    }} className='h-screen sm:h-auto  '>
-        <Navbar/>
- <section>
-        <p className='mt-0 sm:mt-[178px] px-2 py-2 text-lg sm:text-4xl font-semibold  w-60 sm:w-[598px] h-[137px] text-white text-left ml-0 sm:ml-[28px] font-frenchCanon'> 
-      <TypeAnimation
-       sequence={[
-        " WELCOME TO Remed RESEARCH SOLUTIONS Pvt. Ltd" ,2000,
-        "Advancing Clinical Trials with Expertise and Trust. ",1000
-       ]}
-       speed={60}
-       repeat={Infinity}
-       wrapper='div'
-      />
-       
-        </p>
-        <p className=' px-2  py-2 mb-6 w-52 sm:w-[445px] h-[101px] text-left text-sm sm:text-xl ml-0 sm:ml-[28px] font-semibold font-frenchCanon '>
-        We as a Site Management Organization engaged in the service of providing suitable sites and the patient pool to our clients to carry out Clinical trials of new/ marketed drugs
-        </p>
-       
-       <div className='py-4 sm:py-20'>
-       <Link to='About' smooth={true} duration={600}> <button className='bg-[#00FFFF] w-20 sm:w-[131px] h-10 sm:h-[63px] ml-[28px] rounded-lg  flex items-center justify-center font-semibold font-frenchCanon'>Read More</button></Link>
-        
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}
+      className="h-screen sm:h-auto"
+    >
+      <Navbar />
 
-       </div>
-       </section>
+      <section className="flex flex-col justify-center sm:items-start items-center text-center sm:text-left sm:px-6">
+        <div className="mt-16 sm:mt-[178px] px-4 sm:px-0">
+          <p className="text-lg sm:text-4xl font-semibold sm:w-[598px] min-h-[137px] text-black sm:text-white font-frenchCanon">
+            <TypeAnimation
+              sequence={[
+                'WELCOME TO Remed RESEARCH SOLUTIONS Pvt. Ltd', 2000,
+                'Advancing Clinical Trials with Expertise and Trust.', 1000
+              ]}
+              speed={60}
+              repeat={Infinity}
+              wrapper="div"
+            />
+          </p>
+        </div>
 
+        <div className="mt-4 px-4 sm:px-0">
+          <p className="text-sm sm:text-xl font-semibold sm:w-[445px] min-h-[101px] font-frenchCanon">
+            We as a Site Management Organization engage in providing suitable sites and patient pools to our clients for conducting clinical trials of new/marketed drugs.
+          </p>
+        </div>
+
+        <div className="mt-8 sm:mt-20 pb-16">
+          <Link to="About" smooth={true} duration={600}>
+            <button className="bg-[#00FFFF] w-28 sm:w-[131px] h-10 sm:h-[63px] rounded-lg flex items-center justify-center font-semibold font-frenchCanon">
+              Read More
+            </button>
+          </Link>
+        </div>
+      </section>
     </header>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
